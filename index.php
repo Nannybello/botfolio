0769d0b457d25a58e7b9482c281ff32a06c57c07
Nanny Bello <nanny5p2n@gmail.com>
2019-12-22 12:48:16 +0700

    no message

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 
 //echo "test botfolio";
-include 'public/index.php';
+//include 'public/index.php';
+
+
+use Illuminate\Support\Facades\Log;
+
+$content = file_get_contents('php://input');
+
+Log::info(json_encode(["GET" => $_GET, "POST" => $_POST, "CONTENT" => $content, "HEADERS" => getallheaders()]));
