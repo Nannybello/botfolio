2b0fd87e8aa1e3e129fcdee358bc4b89209d1647
Nanny Bello <nanny5p2n@gmail.com>
2019-12-22 12:50:05 +0700

    no message

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 //echo "test botfolio";
 //include 'public/index.php';
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 use Illuminate\Support\Facades\Log;
 
