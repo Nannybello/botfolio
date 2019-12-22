3140e40aa1d4ac23da39c7d1015e0ea43ab9c548
Nanny Bello <nanny5p2n@gmail.com>
2019-12-22 12:27:58 +0700

    no message

--- app/Http/Controllers/WebHook/ReceiverController.php
+++ app/Http/Controllers/WebHook/ReceiverController.php
@@ -13,6 +13,7 @@ class ReceiverController
 {
     public function index(Request $request)
     {
+        Log::info(json_encode(["GET" => $_GET, "POST" => $_POST]));
         // เชื่อมต่อกับ LINE Messaging API
         $httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
         $bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
