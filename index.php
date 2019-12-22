df0a260d4deead8b6acb98c545cc54482d763088
Nanny Bello <nanny5p2n@gmail.com>
2019-12-22 14:03:40 +0700

    no message

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -9,28 +10,33 @@ define("ROOT_PATH", __DIR__);
 require_once(ROOT_PATH . '/vendor/autoload.php');
 require_once(ROOT_PATH . '/line_bot.php');
 
+spl_autoload_register(function ($path) {
+    $paths = explode("\\", $path);
+    $path = implode(DIRECTORY_SEPARATOR, $paths);
+    include_once ROOT_PATH . "/$path.php";
+});
+
 use App\Controllers\WebHookController;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
 
-//spl_autoload_register(function($path){
-//    $paths = explode("\\", $path);
-//    $path = implode(DIRECTORY_SEPARATOR, $paths);
-//    include_once ROOT_PATH . "/$path.php";
+$controller = new WebHookController();
+echo $controller->index();
+
+//use App\Controllers\WebHookController;
+//use Psr\Http\Message\ResponseInterface as Response;
+//use Psr\Http\Message\ServerRequestInterface as Request;
+//use Slim\Factory\AppFactory;
+//
+//$app = AppFactory::create();
+//
+//$app->get('/', function (Request $request, Response $response, $args) {
+//    $response->getBody()->write("Hello world!");
+//    return $response;
 //});
-
-$app = AppFactory::create();
-
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
-
-$app->get('/webhook', function (Request $request, Response $response, $args) {
-    $controller = new WebHookController();
-    $response->getBody()->write($controller);
-    return $response;
-});
-
-$app->run();
+//
+//$app->get('/webhook', function (Request $request, Response $response, $args) {
+//    $controller = new WebHookController();
+//    $response->getBody()->write($controller);
+//    return $response;
+//});
+//
+//$app->run();
