59fca3c0bb912e666eccce033c03b22c24539d9e
Nanny Bello <nanny5p2n@gmail.com>
2019-12-22 13:00:21 +0700

    no message

--- index.php
+++ index.php
@@ -6,8 +6,17 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-use Illuminate\Support\Facades\Log;
+require_once(__DIR__.'/vendor/autoload.php');
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+$logger = new Logger('channel-name');
+$logger->pushHandler(new StreamHandler(__DIR__.'/storage/app.log', Logger::DEBUG));
+//$logger->info('This is a log! ^_^ ');
+//$logger->warning('This is a log warning! ^_^ ');
+//$logger->error('This is a log error! ^_^ ');
 
 $content = file_get_contents('php://input');
 
-Log::info(json_encode(["GET" => $_GET, "POST" => $_POST, "CONTENT" => $content, "HEADERS" => getallheaders()]));
+$logger->info(json_encode(["GET" => $_GET, "POST" => $_POST, "CONTENT" => $content, "HEADERS" => getallheaders()]));
