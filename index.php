7e6a976c69aeaf00cce0b8452a86571f8bfdb7dd
Nanny Bello <nanny5p2n@gmail.com>
2019-12-22 13:08:55 +0700

    no message

--- index.php
+++ index.php
@@ -6,17 +6,41 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once(__DIR__.'/vendor/autoload.php');
+require_once(__DIR__ . '/vendor/autoload.php');
 
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
 $logger = new Logger('channel-name');
-$logger->pushHandler(new StreamHandler(__DIR__.'/storage/app.log', Logger::DEBUG));
-//$logger->info('This is a log! ^_^ ');
-//$logger->warning('This is a log warning! ^_^ ');
-//$logger->error('This is a log error! ^_^ ');
+$logger->pushHandler(new StreamHandler(__DIR__ . '/storage/app.log', Logger::DEBUG));
 
 $content = file_get_contents('php://input');
-
 $logger->info(json_encode(["GET" => $_GET, "POST" => $_POST, "CONTENT" => $content, "HEADERS" => getallheaders()]));
+
+
+//
+
+
+$httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
+$bot = new LINEBot($httpClient, ['channelSecret' => LINE_MESSAGE_CHANNEL_SECRET]);
+
+// แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
+$events = json_decode($content, true);
+
+if (!is_null($events)) {
+    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+    $replyToken = $events['events'][0]['replyToken'];
+}
+// ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
+$textMessageBuilder = new TextMessageBuilder(json_encode($events));
+
+//l ส่วนของคำสั่งตอบกลับข้อความ
+$response = $bot->replyMessage($replyToken ?? "", $textMessageBuilder);
+if ($response->isSucceeded()) {
+    echo 'Succeeded!';
+} else {
+    echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+}
