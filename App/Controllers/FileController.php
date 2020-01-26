7c04fb2f7eec0b2c57f3885f2f8497f602abb513
Nanny Bello <nanny5p2n@gmail.com>
2020-01-26 13:45:02 +0700

    no message

--- App/Controllers/FileController.php
+++ App/Controllers/FileController.php
@@ -4,11 +4,20 @@
 namespace App\Controllers;
 
 use App\Database\Models\FilesInfo;
+use function App\Helpers\makeLink;
+use function App\Helpers\userFileUrl;
 
 class FileController extends BaseController
 {
     public function index(): array
     {
-        return FilesInfo::query()->where('user_id', '=', $this->userId)->get()->toArray();
+        $files = FilesInfo::query()->where('user_id', '=', $this->userId)->get()->toArray();
+        echo json_encode([
+            $files[0]['filename_original'],
+            $files[0]['filetype'] . ", upload at " . $files[0]['created_at'],
+            userFileUrl($files[0]['filename'], $this->userId),
+            makeLink(userFileUrl($files[0]['filename'], $this->userId)),
+        ]);
+        return $files;
     }
 }
