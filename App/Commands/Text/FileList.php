831a24184cc9d998f05cb4c4f31d355e001c970f
Nanny Bello <nanny5p2n@gmail.com>
2020-01-26 12:59:20 +0700

    no message

--- App/Commands/Text/FileList.php
+++ App/Commands/Text/FileList.php
@@ -30,7 +30,32 @@ class FileList extends BaseCommands
 
         $logger = new Logger('channel-name');
         $logger->pushHandler(new StreamHandler(ROOT_PATH . '/storage/command.log', Logger::DEBUG));
-        $logger->alert(json_encode($files));
+        $logger->info(json_encode($files));
+
+
+        try {
+            $textMessageBuilder = new LINEBot\MessageBuilder\TemplateMessageBuilder("img",
+                new LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder(
+                    [
+                        new LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder(
+                            "Title",
+                            "Desc",
+                            "https://botfolio.beautyandballoon.com/storage/user_files/1/20200111-164500.jpg",
+                            [
+                                new LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder(
+                                    "Download", "https://botfolio.beautyandballoon.com/storage/user_files/1/20200111-164500.jpg"
+                                )
+                            ]
+                        ),
+                    ]
+                )
+            );
+            return $this->bot->replyMessage($this->replyToken, $textMessageBuilder);
+        } catch (Exception $e) {
+            $logger = new Logger('channel-name');
+            $logger->pushHandler(new StreamHandler(ROOT_PATH . '/storage/command.log', Logger::DEBUG));
+            $logger->alert($e->getMessage());
+        }
 
         if (!$files) {
             $output = 'ยังไม่มีไฟล์';
