2ff45261cd00c3d676b7a11610c26c84ea88e30e
Nanny Bello <nanny5p2n@gmail.com>
2020-02-16 16:31:07 +0700

    no message

--- dialogflow.php
+++ dialogflow.php
@@ -7,7 +7,7 @@ include 'autoload.php';
 
 $logger = new Logger('channel-name');
 $logger->pushHandler(new StreamHandler(ROOT_PATH . '/storage/dialogflow.log', Logger::DEBUG));
-$logger->log("BEGIN", "-----------------------------------------------------------");
+$logger->info("BEGIN", ["-----------------------------------------------------------"]);
 $logger->info("CONTENT", [
     'GET' => $_GET,
     'POST' => $_POST,
