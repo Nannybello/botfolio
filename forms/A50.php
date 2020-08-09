14ee1c3119f619e8b064da2182384815e2fa3b7f
Nanny Bello <nanny5p2n@gmail.com>
2020-08-09 14:42:44 +0700

    A5

--- forms/A50.php
+++ forms/A50.php
@@ -1,5 +1,30 @@
 <h1>บันทึกข้อความ A5</h1>
 
+<ol>
+    <li>
+        ชื่อ / นามสกุล {{info_first_name}} {{info_last_name}}
+    </li>
+    <li>
+        ชื่อหลักสูตร {{courseName}} <br/>
+        เพื่อ
+        <input type="radio" name="purpose" id="purpose_1" value="training"/>
+        <label for="purpose_1">ฝึกอบรม</label>
+        
+        <input type="radio" name="purpose" id="purpose_2" value="research"/>
+        <label for="purpose_2">ดูงาน</label>
+    </li>
+    <li>
+        สรุปผลโดยย่อ<br/>
+        <input type="radio" name="purposeSummary" id="purposeSummary_1" value="purposeFor"/>
+        <label for="purposeSummary_1">วัตถุประสงค์</label>
+        {{purposeTrainingSummary}}
+
+        <input type="radio" name="purposeSummary" id="purposeSummary_2" value="courseSummary"/>
+        <label for="purposeSummary_2">วัตถุประสงค์</label>
+        {{purposeTrainingSummary}}
+    </li>
+</ol>
+
 <div>
     <p>ส่วนราชการ สาขาวิชา {{info_major}} คณะ {{info_faculty}}</p>
     <p>ที่ {{at}} วันที่ {{date}}</p>
@@ -23,12 +48,14 @@
     </tr>
     </thead>
     <tbody>
+    <!--1-->
     <tr>
         <td>1.สามารถนำความรู้มาใช้ ...</td>
         <td>{{yes_1}}</td>
         <td>{{no_1}}</td>
         <td>{{how_1}}</td>
     </tr>
+    <!--1-->
     </tbody>
 </table>
 
