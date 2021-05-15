53c19c5c057bbed243bc77be606b857d693d3a0d
Nanny Bello <nanny5p2n@gmail.com>
2021-05-15 13:43:45 +0700

    store upload file and file info user owner

--- App/Controllers/Web/UploadFile.php
+++ App/Controllers/Web/UploadFile.php
@@ -35,8 +35,50 @@ class UploadFile
         $user = User::fromToken($token);
         print_r($user);
         print_r($_FILES);
-    }   
-    
+
+        $file = $_FILES['fileToUpload'];
+
+        $file_name = $file['name'];
+        $file_size = $file['size'];
+        $file_tmp = $file['tmp_name'];
+        $file_type = $file['type'];
+
+        $errors = [];
+//        $file_ext = strtolower(end(explode('.', $file['name'])));
+//
+//        $extensions = array("jpeg", "jpg", "png");
+//
+//        if (in_array($file_ext, $extensions) === false) {
+//            $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
+//        }
+//
+//        if ($file_size > 2097152) {
+//            $errors[] = 'File size must be excately 2 MB';
+//        }
+
+        if (!empty($errors)) {
+            print_r($errors);
+            return;
+        }
+
+        move_uploaded_file($file_tmp, $this->getUserFileStoragePath($file_name, $user->id));
+
+        $s = explode('.', $file_name);
+        $ext = end($s);
+        $filename = date('Ymd-His') . '.' . $ext;
+
+
+        $rec = new FileInfo();
+        $rec->filename = $filename;
+        $rec->filename_original = $file_name;
+        $rec->filetype = $ext;
+        $rec->user_id = $user->id;
+        $rec->created_at = date('Y-m-d H:i:s');
+        $rec->save();
+        
+        echo "Upload Success";
+    }
+
     function getUserFileStoragePath(string $filename, string $userId): string
     {
         return ROOT_PATH . "/storage/user_files/$userId/$filename";
